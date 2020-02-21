d29987fb388b1709828c27c5c30ae98b87ace790
Zeljko Bilbija <bilbijazeljko@gmail.com>
2020-02-21 10:58:42 +0100

    3.

---
+++ src/com/interfacemockup/Questions.java
@@ -0,0 +1,18 @@
+package com.interfacemockup;
+import java.util.*;
+
+
+public class Questions {
+
+    // invariant: answer is in [lo, hi)
+
+
+    public static void main(String[] args) {
+        int a = 7;
+
+
+        System.out.println(args.length);
+
+        System.out.println(a * 5);
+    }
+}
